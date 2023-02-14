32940f83c775996c6b269f7ea4abfd0bd07411be
riulenn <riuqab@gmail.com>
2023-02-14 16:35:58 +0800

    i added a try button chuchu

--- Equivalent Grades and GWA Calculator/Form1.Designer.cs
+++ Equivalent Grades and GWA Calculator/Form1.Designer.cs
@@ -39,8 +39,9 @@
             this.titleBar.BackColor = System.Drawing.Color.Moccasin;
             this.titleBar.Controls.Add(this.btnClose);
             this.titleBar.Location = new System.Drawing.Point(0, 0);
+            this.titleBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(950, 35);
+            this.titleBar.Size = new System.Drawing.Size(1267, 43);
             this.titleBar.TabIndex = 0;
             this.titleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.titleBar_MouseDown);
             this.titleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.titleBar_MouseMove);
@@ -51,10 +52,10 @@
             this.btnClose.FlatAppearance.BorderSize = 0;
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(915, -2);
+            this.btnClose.Location = new System.Drawing.Point(1220, -2);
             this.btnClose.Margin = new System.Windows.Forms.Padding(0);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(35, 37);
+            this.btnClose.Size = new System.Drawing.Size(47, 46);
             this.btnClose.TabIndex = 0;
             this.btnClose.Text = "X";
             this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -63,22 +64,24 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(427, 153);
+            this.button1.Location = new System.Drawing.Point(261, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(950, 550);
+            this.ClientSize = new System.Drawing.Size(1267, 677);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.titleBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
