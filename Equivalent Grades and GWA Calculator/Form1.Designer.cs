7df88ab6cdfbfd7233303b16b4c4a87405a52ee5
Mark Decena <markangelo091601@gmail.com>
2023-02-14 16:24:24 +0800

    test

--- Equivalent Grades and GWA Calculator/Form1.Designer.cs
+++ Equivalent Grades and GWA Calculator/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.titleBar = new System.Windows.Forms.Panel();
             this.btnClose = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.titleBar.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,12 +61,22 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(950, 550);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.titleBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
@@ -82,6 +93,7 @@
 
         private System.Windows.Forms.Panel titleBar;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button button1;
     }
 }
 
